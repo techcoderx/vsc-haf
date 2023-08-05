eb8cc55bdd7b0e7fef650069a3b5d1fdd7f4453e
techcoderx <engtianxi@outlook.com>
2023-08-05 16:19:46 +0800

    get block by hash api

--- src/sql/create_apis.sql
+++ src/sql/create_apis.sql
@@ -24,4 +24,34 @@ END
 $function$
 LANGUAGE plpgsql STABLE;
 
--- The rest of PostgREST API methods goes here
+CREATE OR REPLACE FUNCTION vsc_api.get_block_by_hash(block_hash VARCHAR)
+RETURNS jsonb
+AS
+$function$
+DECLARE
+    _announced_in_op BIGINT;
+    _block_id INTEGER;
+    _announced_in_tx_id BIGINT;
+    _announced_in_tx TEXT;
+BEGIN
+    SELECT id, announced_in_op INTO _block_id, _announced_in_op
+        FROM vsc_app.blocks
+        WHERE vsc_app.blocks.block_hash = block_hash
+        LIMIT 1;
+    SELECT op_id INTO _announced_in_tx_id
+        FROM vsc_app.l1_operations
+        WHERE vsc_app.l1_operations.id = _announced_in_op;
+    SELECT htx.trx_hash::TEXT INTO _announced_in_tx FROM hive.transactions_view htx
+        JOIN hive.operations_view ON
+            hive.operations_view.block_num = hive.transactions_view.block_num AND
+            hive.operations_view.trx_in_block = hive.transactions_view.trx_in_block
+        WHERE hive.operations_view.id = _announced_in_tx_id;
+    
+    RETURN jsonb_build_object(
+        'id', _block_id,
+        'block_hash', block_hash,
+        'announced_in_tx', _announced_in_tx
+    )
+END
+$function$
+LANGUAGE plpgsql STABLE;

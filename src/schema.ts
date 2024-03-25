277c58518026869d630dd575427b32acec02103a
techcoderx <engtianxi@outlook.com>
2024-03-25 16:27:25 +0800

    fill initial values before state provider update

--- src/schema.ts
+++ src/schema.ts
@@ -192,32 +192,8 @@ const schema = {
         // setup app tables
         await db.client.query(fs.readFileSync(__dirname+'/sql/create_tables.sql','utf-8'))
 
-        // inheritance for forking app
-        for (let t in HAF_TABLES)
-            await db.client.query(`ALTER TABLE ${SCHEMA_NAME}.${HAF_TABLES[t]} INHERIT hive.${SCHEMA_NAME};`)
-
-        // use 'accounts' state provider
-        await db.client.query(`SELECT hive.app_state_provider_import('ACCOUNTS',$1);`,[APP_CONTEXT])
-        logger.info('Imported accounts state provider')
-
-        // detach app context
-        await context.detach()
-
-        // start block
-        let startBlock = Math.max(START_BLOCK-1,0)
-        await db.client.query('START TRANSACTION;')
-        if (startBlock > 0) {
-            logger.info('Updating state providers to starting block...')
-            let start = new Date().getTime()
-            await db.client.query('SELECT hive.app_state_providers_update($1,$2,$3);',[0,startBlock,APP_CONTEXT])
-            logger.info('State providers updated in',(new Date().getTime()-start),'ms')
-        }
-        await db.client.query(`UPDATE ${SCHEMA_NAME}.state SET last_processed_block=$1;`,[startBlock])
-        await db.client.query(`SELECT hive.app_set_current_block_num($1,$2);`,[APP_CONTEXT,startBlock])
-        await db.client.query('COMMIT;')
-        logger.info('Set last processed block to #'+(startBlock))
-
         // fill with initial values
+        let startBlock = Math.max(START_BLOCK-1,0)
         await db.client.query('START TRANSACTION;')
         await db.client.query(`INSERT INTO ${SCHEMA_NAME}.state(last_processed_block, db_version) VALUES($1, $2);`,[startBlock,DB_VERSION])
         await db.client.query(`INSERT INTO ${SCHEMA_NAME}.l1_operation_types(op_name) VALUES('announce_node');`)
@@ -231,6 +207,30 @@ const schema = {
         await db.client.query(`INSERT INTO ${SCHEMA_NAME}.withdrawal_status(name) VALUES('completed');`)
         await db.client.query(`INSERT INTO ${SCHEMA_NAME}.withdrawal_status(name) VALUES('failed');`)
         await db.client.query('COMMIT;')
+
+        // inheritance for forking app
+        for (let t in HAF_TABLES)
+            await db.client.query(`ALTER TABLE ${SCHEMA_NAME}.${HAF_TABLES[t]} INHERIT hive.${SCHEMA_NAME};`)
+
+        // use 'accounts' state provider
+        await db.client.query(`SELECT hive.app_state_provider_import('ACCOUNTS',$1);`,[APP_CONTEXT])
+        logger.info('Imported accounts state provider')
+
+        // detach app context
+        await context.detach()
+
+        // massive state provider update
+        await db.client.query('START TRANSACTION;')
+        if (startBlock > 0) {
+            logger.info('Updating state providers to starting block...')
+            let start = new Date().getTime()
+            await db.client.query('SELECT hive.app_state_providers_update($1,$2,$3);',[0,startBlock,APP_CONTEXT])
+            logger.info('State providers updated in',(new Date().getTime()-start),'ms')
+        }
+        await db.client.query(`UPDATE ${SCHEMA_NAME}.state SET last_processed_block=$1;`,[startBlock])
+        await db.client.query(`SELECT hive.app_set_current_block_num($1,$2);`,[APP_CONTEXT,startBlock])
+        await db.client.query('COMMIT;')
+        logger.info('Set last processed block to #'+(startBlock))
 
         // create relevant functions
         await schema.createFx()
@@ -250,43 +250,42 @@ const schema = {
         let constraint = await db.client.query('SELECT * FROM information_schema.constraint_column_usage WHERE constraint_name=$1',[fk])
         return constraint.rowCount! > 0
     },
-    fkCreate: async () => {
-        for (let fk in HAF_FKS) {
-            logger.info('Creating FK',fk)
+    fkCreate: async (fks: FKS_TYPE = HAF_FKS) => {
+        for (let fk in fks) {
             if (await schema.fkExists(fk)) {
                 logger.info('FK',fk,'already exists, skipping')
                 continue
             }
             let start = new Date().getTime()
-            await db.client.query(`ALTER TABLE ${HAF_FKS[fk].table} ADD CONSTRAINT ${fk} FOREIGN KEY(${HAF_FKS[fk].fk}) REFERENCES ${HAF_FKS[fk].ref} DEFERRABLE INITIALLY DEFERRED;`)
+            await db.client.query(`ALTER TABLE ${fks[fk].table} ADD CONSTRAINT ${fk} FOREIGN KEY(${fks[fk].fk}) REFERENCES ${fks[fk].ref} DEFERRABLE INITIALLY DEFERRED;`)
             logger.info('FK',fk,'created in',(new Date().getTime()-start),'ms')
         }
     },
-    fkDrop: async () => {
-        for (let fk in HAF_FKS)
+    fkDrop: async (fks: FKS_TYPE = HAF_FKS) => {
+        for (let fk in fks)
             if (await schema.fkExists(fk)) {
                 logger.info('Droping FK',fk)
                 let start = new Date().getTime()
-                await db.client.query(`ALTER TABLE ${HAF_FKS[fk].table} DROP CONSTRAINT IF EXISTS ${fk};`)
+                await db.client.query(`ALTER TABLE ${fks[fk].table} DROP CONSTRAINT IF EXISTS ${fk};`)
                 logger.info('FK',fk,'dropped in',(new Date().getTime()-start),'ms')
             }
     },
     indexExists: async (index_name: string): Promise<boolean> => {
         return (await db.client.query('SELECT * FROM pg_indexes WHERE schemaname=$1 AND indexname=$2',[SCHEMA_NAME, index_name])).rowCount! > 0
     },
-    indexCreate: async () => {
-        for (let idx in INDEXES) {
+    indexCreate: async (idxs: INDEXES_TYPE = INDEXES) => {
+        for (let idx in idxs) {
             if (await schema.indexExists(idx)) {
                 logger.info('Index',idx,'already exists, skipping')
                 continue
             }
             let start = new Date().getTime()
-            await db.client.query(`CREATE INDEX IF NOT EXISTS ${idx} ON ${INDEXES[idx].table_name}(${INDEXES[idx].columns.map(x => x.col_name+' '+Ordering[x.order]).join(',')}) ${INDEXES[idx].condition?'WHERE '+INDEXES[idx].condition:''};`)
+            await db.client.query(`CREATE INDEX IF NOT EXISTS ${idx} ON ${idxs[idx].table_name}(${idxs[idx].columns.map(x => x.col_name+' '+Ordering[x.order]).join(',')}) ${idxs[idx].condition?'WHERE '+idxs[idx].condition:''};`)
             logger.info('Index',idx,'created in',(new Date().getTime()-start),'ms')
         }
     },
-    indexDrop: async () => {
-        for (let idx in INDEXES) {
+    indexDrop: async (idxs: INDEXES_TYPE = INDEXES) => {
+        for (let idx in idxs) {
             if (await schema.indexExists(idx)) {
                 let start = new Date().getTime()
                 await db.client.query(`DROP INDEX IF EXISTS ${idx} CASCADE;`)

f73ffd5dd00945293849a0f9410aab2f52d408e7
techcoderx <engtianxi@outlook.com>
2024-03-28 10:13:12 +0800

    user fix

--- src/processor.ts
+++ src/processor.ts
@@ -46,7 +46,7 @@ const processor = {
                     valid: true,
                     id: op.id,
                     ts: op.timestamp,
-                    user: requiresActiveAuth ? parsed.value.required_auths[0] : parsed.value.required_posting_auths[0],
+                    user: user,
                     block_num: op.block_num,
                     trx_in_block: op.trx_in_block,
                     op_pos: op.op_pos,

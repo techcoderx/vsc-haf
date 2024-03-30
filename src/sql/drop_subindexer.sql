72533b83f90662b9cf336a5839cf1c9f97cb8d7d
techcoderx <engtianxi@outlook.com>
2024-03-30 15:45:14 +0800

    drop constraints on subindexer drop

--- src/sql/drop_subindexer.sql
+++ src/sql/drop_subindexer.sql
@@ -1,3 +1,21 @@
+ALTER TABLE vsc_app.blocks DROP CONSTRAINT IF EXISTS block_proposed_in_op_fk;
+ALTER TABLE vsc_app.blocks DROP CONSTRAINT IF EXISTS block_proposer_fk;
+ALTER TABLE vsc_app.election_results DROP CONSTRAINT IF EXISTS election_proposed_in_op_fk;
+ALTER TABLE vsc_app.election_results DROP CONSTRAINT IF EXISTS election_proposer_fk;
+ALTER TABLE vsc_app.election_result_members DROP CONSTRAINT IF EXISTS elected_members_epoch_fk;
+ALTER TABLE vsc_app.election_result_members DROP CONSTRAINT IF EXISTS elected_members_user_id_fk;
+ALTER TABLE vsc_app.l1_txs DROP CONSTRAINT IF EXISTS l1_txs_op_id_fk;
+ALTER TABLE vsc_app.l1_txs DROP CONSTRAINT IF EXISTS l1_txs_details_fk;
+ALTER TABLE vsc_app.l1_tx_multiauth DROP CONSTRAINT IF EXISTS l1_tx_multiauth_tx_id;
+ALTER TABLE vsc_app.l1_tx_multiauth DROP CONSTRAINT IF EXISTS l1_tx_multiauth_user_id_fk;
+ALTER TABLE vsc_app.l2_txs DROP CONSTRAINT IF EXISTS l2_txs_block_num_fk;
+ALTER TABLE vsc_app.l2_txs DROP CONSTRAINT IF EXISTS l2_txs_details_fk;
+ALTER TABLE vsc_app.l2_tx_multiauth DROP CONSTRAINT IF EXISTS l2_tx_multiauth_tx_id;
+ALTER TABLE vsc_app.l2_tx_multiauth DROP CONSTRAINT IF EXISTS l2_tx_multiauth_did_fk;
+ALTER TABLE vsc_app.transactions DROP CONSTRAINT IF EXISTS contract_call_contract_id_fk;
+ALTER TABLE vsc_app.anchor_refs DROP CONSTRAINT IF EXISTS anchor_refs_block_num_fk;
+ALTER TABLE vsc_app.anchor_ref_txs DROP CONSTRAINT IF EXISTS anchor_ref_txs_ref_id_fk;
+
 TRUNCATE TABLE vsc_app.subindexer_state;
 TRUNCATE TABLE vsc_app.election_result_members;
 TRUNCATE TABLE vsc_app.election_results;

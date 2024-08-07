d109d84ffcbfd8f3890b342f8e7f47f380f0156f
techcoderx <engtianxi@outlook.com>
2024-08-07 12:10:09 +0800

    truncate new tables on subindexer drop

--- src/sql/drop_subindexer.sql
+++ src/sql/drop_subindexer.sql
@@ -26,12 +26,15 @@ TRUNCATE TABLE vsc_app.l1_txs;
 TRUNCATE TABLE vsc_app.l1_tx_multiauth;
 TRUNCATE TABLE vsc_app.l2_txs;
 TRUNCATE TABLE vsc_app.l2_tx_multiauth;
+TRUNCATE TABLE vsc_app.transfers;
+TRUNCATE TABLE vsc_app.events;
 TRUNCATE TABLE vsc_app.contract_calls;
 TRUNCATE TABLE vsc_app.anchor_refs;
 TRUNCATE TABLE vsc_app.anchor_ref_txs;
 
 SELECT setval(pg_get_serial_sequence('vsc_app.l2_blocks', 'id'), 1, false);
 SELECT setval(pg_get_serial_sequence('vsc_app.contract_calls', 'id'), 1, false);
+SELECT setval(pg_get_serial_sequence('vsc_app.transfers', 'id'), 1, false);
 SELECT setval(pg_get_serial_sequence('vsc_app.anchor_refs', 'id'), 1, false);
 
 UPDATE vsc_app.withdrawal_request SET
